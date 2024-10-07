26e9dc7b7d8c0bde827724971a6ea98233cb22c6
shubham-navik <112787977+shubham-navik@users.noreply.github.com>
2024-10-07 20:28:08 +0530

    updated code

--- snakeLadder.cpp
+++ snakeLadder.cpp
@@ -1,56 +1,53 @@
-
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Player{
-    public:
+class Player {
+public:
     string Name;
     int position;
-    Player(string name):Name(name),position(0){}
+    Player(string name) : Name(name), position(0) {}
 };
 
-class Ladder{
-    public:
+class Ladder {
+public:
     int size;
     vector<int> board;
     set<Player *> st;
 
-    Ladder(int size){
+    Ladder(int size) {
         this->size = size;
-        for (int i = 0; i <= size;i++){
+        for (int i = 0; i <= size; i++) {
             board.push_back(i);
         }
     }
 
-    void addLadderAndSnake(int start,int end,string type){
-        if(type == "snake"){
+    void addLadderAndSnake(int start, int end, string type) {
+        if (type == "snake") {
             board[start] = end;
-        }else{
+        } else {
             board[start] = end;
         }
-
     }
 
-    void addPlayer(Player* p){
+    void addPlayer(Player *p) {
         st.insert(p);
     }
 
-    int forword(Player* p,int move){
-
-        if(st.find(p) == st.end()){
-            cout << "Player " << p->Name << "not exist\n";
+    int forward(Player *p, int move) {
+        if (st.find(p) == st.end()) {
+            cout << "Player " << p->Name << " does not exist\n";
             return 0;
         }
 
-        if(p->position + move > 100){
+        if (p->position + move > 100) {
             return 0;
         }
-        //forwrd the player with move
+
+        // Move the player
         p->position = p->position + move;
         p->position = board[p->position];
 
-        if(p->position == 100){
+        if (p->position == 100) {
             cout << p->Name << " won the first Position\n";
             return 1;
         }
@@ -58,38 +55,33 @@ class Ladder{
         return 0;
     }
 
-    void showLadder(){
+    void showLadder() {
         cout << "________________________________________________________________\n";
-        for (int i = 1; i <= size;i++){
+        for (int i = 1; i <= size; i++) {
             bool found = false;
             string name;
-            for(auto player :st){
-                if(player->position == i){
-                    // cout << " "<<player->Name[0]<<" ";
-                    name = player->Name[0];
+            for (auto player : st) {
+                if (player->position == i) {
+                    name = player->Name;
                     found = true;
                 }
-
             }
-            if(found)
-                cout <<name<<"\t";
-            else{
+            if (found)
+                cout << name << "\t";
+            else {
                 cout << board[i] << "\t";
             }
-            if(i%10==0)
+            if (i % 10 == 0)
                 cout << endl;
         }
         cout << "________________________________________________________________\n";
-
     }
-
-
 };
 
-int main(){
-
+int main() {
     Ladder *L = new Ladder(100);
 
+    // Add ladders and snakes
     L->addLadderAndSnake(1, 7, "lad");
     L->addLadderAndSnake(14, 67, "lad");
     L->addLadderAndSnake(51, 75, "lad");
@@ -102,35 +94,35 @@ int main(){
 
     Player *p1 = new Player("Xhubham");
     Player *p2 = new Player("Zatyam");
-    // Player *p3 = new Player("ashis");
-    // Player *p4 = new Player("mayank");
+    Player *p3 = new Player("Matyam");
 
     L->addPlayer(p1);
     L->addPlayer(p2);
 
     L->showLadder();
 
-    while(1){
-        bool start = true;
-        for (int i = 1; i <= 2; i++)
-        {
-            if(!start)break;
-            srand(static_cast<unsigned>(time(0)));
+    srand(static_cast<unsigned>(time(0))); // Seed the random number generator once outside the loop
 
-            // Define the range
+    while (1) {
+        bool start = true;
+        for (int i = 1; i <= 2; i++) {
+            if (!start) break;
+
             int lower = 1;
             int upper = 6;
 
-            // Generate a random number in the range [lower, upper]
+            // Generate a random move
             int move = lower + rand() % (upper - lower + 1);
-            if( i%2 == 0 && L->forword(p1,move)){
+            cout << "Move: " << move << endl;
+
+            if (i % 2 != 0 && L->forward(p1, move)) {  // Player 1 moves on odd turns
                 start = false;
-            }else if(i%2 != 0 && L->forword(p2,move)){
+            } else if (i % 2 == 0 && L->forward(p2, move)) {  // Player 2 moves on even turns
                 start = false;
             }
         }
         L->showLadder();
-        if(!start){
+        if (!start) {
             cout << "Game End \n";
             break;
         }
